d4ae0868528f3867303e3c1b9a520435d1655b98
LikeAPhoenix <h_h-hxd@qq.com>
2025-10-29 20:33:06 +0800

    更新 serve.go 文件以支持通过命令行参数指定端口

--- tools/serve.go
+++ tools/serve.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
 
 func main() {
-	port := "8000"
+	port := flag.String("port", "8000", "port to listen on")
 	publicDir := "public"
-	fmt.Printf("Serving Go by Example at http://127.0.0.1:%s\n", port)
-	http.ListenAndServe(":"+port, http.FileServer(http.Dir(publicDir)))
+	flag.Parse()
+	fmt.Printf("Serving Go by Example at http://127.0.0.1:%s\n", *port)
+	http.ListenAndServe(":"+*port, http.FileServer(http.Dir(publicDir)))
 }
